b5f3fdd1ff4d15c5b4e749aa2db8170d6462608d
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-13 08:43:53 -0300

    class props operation btn

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -3,6 +3,6 @@ import './Button.css'
 
 export default props =>{
     return(
-    <button className='button'>{props.label}</button>
+    <button className={`button ${props.operation ? 'operation' : ''}`}>{props.label}</button>
     )
 }
