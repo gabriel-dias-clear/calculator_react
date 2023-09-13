21d2693c04c264490c660b2b8ef2b072f6331b4e
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-13 09:07:20 -0300

    button component adapted

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>{
-    return(
-    <button className={`button ${props.operation ? 'operation' : ''}`}>{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
     )
 }
